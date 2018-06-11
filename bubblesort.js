73920b58320162d8bb503aa81cda1559bff5784f
Lucas Levin <lucaslevin1@gmail.com>
2018-06-11 13:53:42 -0400

    refactored and added another test

--- bubblesort.js
+++ bubblesort.js
@@ -1,34 +1,32 @@
 function bubbleSort(array) {
     let newArray = [array[0]];
-    let arrayHolder = [];
-    
-    array.forEach(function(x){
-        arrayHolder.push(x);
-    })
 
-    for(let i = array.length; i > 0; i--){
-        debugger;
-        for(let j = 0; j < i - 1; j++){
+    for (let i = array.length; i > 0; i--){
+
+        for (let j = 0; j < i - 1; j++){
             let test = newArray.pop();
-            let test2 = swap(test, arrayHolder[j+1])
+            let test2 = swap(test, array[j + 1])
             newArray = newArray.concat(test2);
-            
-            
         }
-        newArray = newArray.concat(arrayHolder.slice(i))
-        arrayHolder = [];
-        newArray.forEach(function(x){
-            arrayHolder.push(x);
-        })
-        newArray = [arrayHolder[0]];
+
+        newArray = newArray.concat(array.slice(i))
+        array = [];
+        pushFunc.call(newArray, array);
+        newArray = [array[0]];
     }
-    return arrayHolder;
+    return array;
   }
 
 function swap (ind1, ind2){
-    if(ind1 > ind2){
+    if (ind1 > ind2){
         return [ind2, ind1]
-    } else{
+    } else {
         return [ind1, ind2]
     }
+}
+
+function pushFunc(arr){
+    this.forEach(function(x){
+        arr.push(x);
+    })
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,8 +1,13 @@
 describe('Bubble Sort', function(){
-    it('handles an empty array', function(){
+  it('handles an empty array', function(){
       expect( bubbleSort([]) ).toEqual( [] );
     });
+
     it('works!', function(){
-        expect( bubbleSort([8, 7, 9 ,1]) ).toEqual( [1, 7, 8, 9] );
-      });
+      expect( bubbleSort([8, 7, 9 ,1]) ).toEqual( [1, 7, 8, 9] );
+    });
+
+      it('works with longer array!', function(){
+      expect( bubbleSort([2, 8, 3, 1, 9, 8, 7, 9 ,1]) ).toEqual( [1, 1, 2, 3, 7, 8, 8, 9, 9] );
+    });
   });
