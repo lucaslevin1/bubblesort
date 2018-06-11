54c64399d202306738b717651ffdc32639a52c63
Lucas Levin <lucaslevin1@gmail.com>
2018-06-11 16:29:11 -0400

    optimized?

--- mergesort.js
+++ mergesort.js
@@ -1,24 +1,37 @@
 function split (arr){
-    let num = Math.round(arr.length /2);
-    let arr1 = arr.slice(0, num);
-    let arr2 = arr.slice(num);
-    return [arr1, arr2];
+    let num = Math.round(arr.length / 2);
+    return [arr.slice(0, num), arr.slice(num)];
 }
 function merge (arr1, arr2){
-    return arr1.concat(arr2).sort()
+    return arr1.concat(arr2).sort();
 }
 function mergeSort(array) {
-    let arr1 = split(array)[0]
-    let arr2 = split(array)[1]
+    let arrSplit = split(array),
+        arr1 = arrSplit[0],
+        arr2 = arrSplit[1],
+        arr1Length = one(arr1),
+        arr2Length = one(arr2),
+        arr1MS,
+        arr2MS;
 
-    if (arr1.length === 1 && arr2.length === 1){
+    if (!arr1Length) arr1MS = mergeSort(arr1);
+    if (!arr2Length) arr2MS  = mergeSort(arr2);
+
+    if (arr1Length && arr2Length){
         return merge(arr1, arr2);
-    } else if (arr1.length === 1 && arr2.length > 1){
-        return merge(arr1, mergeSort(arr2));
-    } else if (arr1.length > 1 && arr2.length === 1){
-        return merge(mergeSort(arr1), arr2);
+    } else if (arr1Length){
+        return merge(arr1, arr1MS);
+    } else if (arr2Length){
+        return merge(arr1MS, arr2);
     } else {
-        return merge(mergeSort(arr1), mergeSort(arr2));
+        return merge(arr1MS, arr2MS);
     }
-  
-  }
+}
+
+function one(array){
+    if (array.length === 1){
+        return true;
+    } else {
+        return false;
+    }
+}
